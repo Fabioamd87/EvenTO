b811f7150344a4377a03ea5cadcad958d53657d2
Fabio Sasso <fabioamd87@gmail.com>
2026-02-04 20:02:55 +0100

    more response

--- events.php
+++ events.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eventi</title>
+</head>
+<body>
+
+
 <?php
 include 'db.php';
 session_start();
