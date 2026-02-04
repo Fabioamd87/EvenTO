829b450d8cb61cbf9bbf1a02e84225520c974677
Fabio Sasso <fabioamd87@gmail.com>
2026-02-04 20:05:56 +0100

    more response

--- event.php
+++ event.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eventi</title>
+</head>
+<body>
+
+<!-- si puo mettere il titolo della pagina con il nome dell evento -->
+
 <?php
 include 'db.php';
 session_start();
